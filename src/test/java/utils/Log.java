3b51b62ae38b6923f32a6ccc5468af09ff87cd88
am136qa <168574547+am136qa@users.noreply.github.com>
2024-04-30 14:39:19 -0400

    Update Log.java

--- src/test/java/utils/Log.java
+++ src/test/java/utils/Log.java
@@ -4,11 +4,11 @@ import org.apache.log4j.Logger;
 
 public class Log {
 
-    // Initialize Log4j logs
+
 
     private static Logger Log = Logger.getLogger(Log.class.getName());
 
-    // This is to print log for the beginning of the test case, as we usually run so many test cases as a test suite
+   
 
     public static void startTestCase(String sTestCaseName){
 
@@ -24,8 +24,7 @@ public class Log {
 
     }
 
-    //This is to print log for the ending of the test case
-
+    
     public static void endTestCase(String sTestCaseName){
 
         Log.info("XXXXXXXXXXXXXXXXXXXXXX             "+"-E---N---D-"+"             XXXXXXXXXXXXXXXXXXXXXX");
@@ -40,7 +39,7 @@ public class Log {
 
     }
 
-    // Need to create these methods, so that they can be called
+
 
     public static void info(String message) {
 
