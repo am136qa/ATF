70e7f480f3d2e3945d22043dc2173233030d0036
am136qa <168574547+am136qa@users.noreply.github.com>
2024-04-30 14:22:07 -0400

    Update AddEmployeeSteps.java

--- src/test/java/steps/AddEmployeeSteps.java
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -23,33 +23,27 @@ public class AddEmployeeSteps extends CommonMethods {
 
     @When("user clicks on PIM option")
     public void user_clicks_on_pim_option() {
-        //  WebElement pimOption = driver.findElement(By.id("menu_pim_viewPimModule"));
-        //   pimOption.click();
+       
         click(dashboardPage.pimOption);
     }
 
     @When("user clicks on add employee button")
     public void user_clicks_on_add_employee_button() {
-        //   WebElement addEmployeeBUtton = driver.findElement(By.id("menu_pim_addEmployee"));
-        // addEmployeeBUtton.click();
+       
         click(dashboardPage.addEmployeeButton);
     }
 
     @When("user enters firstname and lastname")
     public void user_enters_firstname_and_lastname() {
-        //  WebElement firstNameTextField = driver.findElement(By.id("firstName"));
-        // firstNameTextField.sendKeys("aendro");
+    
         sendText("aendro", addEmployeePage.firstNameField);
 
-        // WebElement lastNameTextField = driver.findElement(By.id("lastName"));
-        // lastNameTextField.sendKeys("farewell");
         sendText("farewell", addEmployeePage.lastNameField);
     }
 
     @When("user clicks on save button")
     public void user_clicks_on_save_button() {
-        //   WebElement saveButton = driver.findElement(By.id("btnSave"));
-        // saveButton.click();
+
         click(addEmployeePage.saveButton);
     }
 
@@ -81,24 +75,21 @@ public class AddEmployeeSteps extends CommonMethods {
     @When("user enters firstname and middlename and lastname and verify employee has added")
     public void user_enters_firstname_and_middlename_and_lastname_and_verify_employee_has_added
             (io.cucumber.datatable.DataTable dataTable) {
-        //we need list of maps to get multiple values from datatable which is coming
-        // from feature file
+       
         List<Map<String, String>> employeeNames = dataTable.asMaps();
 
         for (Map<String, String> employee : employeeNames
         ) {
-            //getting the values against the key in map
+  
             String firstNameValue = employee.get("firstName");
             String middleNameValue = employee.get("middleName");
             String lastNameValue = employee.get("lastName");
 
-            //filling the name in the fields
+
             sendText(firstNameValue, addEmployeePage.firstNameField);
             sendText(middleNameValue, addEmployeePage.middleNameField);
             sendText(lastNameValue, addEmployeePage.lastNameField);
             click(addEmployeePage.saveButton);
-            //after adding one employee, we will add another employee
-            //for this, we are clicking on add employee button in the loop ittself
             click(dashboardPage.addEmployeeButton);
         }
     }
@@ -106,19 +97,17 @@ public class AddEmployeeSteps extends CommonMethods {
     @When("user adds multiple employees using excel from {string} and verify it")
     public void user_adds_multiple_employees_using_excel_from_and_verify_it
             (String sheetName) throws InterruptedException {
-        //here we are getting the data from excel file using parameters
+   
         List<Map<String, String>> newEmployees =
                 ExcelReader.read(sheetName, Constants.EXCEL_READER_PATH);
 
         Iterator<Map<String, String>> itr = newEmployees.iterator();
 
-        //it will check whether we have new element/value or not
         while (itr.hasNext()) {
 
-            //in this map, we have data from every single employee one by one it will give us that data
+       
             Map<String, String> mapNewEmp = itr.next();
-            //we are filling the employee data now using mapNewEmp variable
-            //BATCH 16, KEYS WHAT WE ARE PASSING HERE SHOULD MATCH WITH THE KEYS IN EXCEL
+
             sendText(mapNewEmp.get("firstName"), addEmployeePage.firstNameField);
             sendText(mapNewEmp.get("lastName"), addEmployeePage.lastNameField);
             sendText(mapNewEmp.get("middleName"), addEmployeePage.middleNameField);
@@ -132,36 +121,33 @@ public class AddEmployeeSteps extends CommonMethods {
             sendText(mapNewEmp.get("password"), addEmployeePage.passwordTextFieldBox);
             sendText(mapNewEmp.get("confirmPassword"), addEmployeePage.confirmPasswordBox);
 
-            //here we are fetching the employee id from the UI using get attribute method
+   
             String empIdValue = addEmployeePage.employeeIdField.getAttribute("value");
             Assert.assertTrue(addEmployeePage.saveButton.isDisplayed());
             click(addEmployeePage.saveButton);
             Thread.sleep(3000);
-            //we have to verify that the employee has been added
+
             click(dashboardPage.empListOption);
-            //searching the employee using emp id which we just got
+ 
             sendText(empIdValue, employeeSearchPage.idTextField);
             click(employeeSearchPage.searchButton);
 
-            //print the value from the table row
+
             List<WebElement> rowData = driver.findElements(By.xpath("//table[@id='resultTable']/tbody/tr"));
             for (int i = 0; i < rowData.size(); i++) {
                 System.out.println("I am inside the loop");
-                //it will return one by one all the data from the row
+         
                 String rowText = rowData.get(i).getText();
-                //it will print the complete row data
-                //output of this will be empid firstname middlename lastname
+         
                 System.out.println(rowText);
-                //we have to verify this data against the data coming from excel
+   
 
                 String expectedData = empIdValue + " " + mapNewEmp.get("firstName") + " " +
                         mapNewEmp.get("middleName") + " " + mapNewEmp.get("lastName");
 
                 Assert.assertEquals(expectedData, rowText);
-                //you can use below code too to verify the data
-                //  Assert.assertTrue(expectedData.equals(rowText));
+               
             }
-            //to add more employees we need to click on add employee button
             click(dashboardPage.addEmployeeButton);
         }
 
