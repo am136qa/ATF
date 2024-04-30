3bbca2a54753deb249025cf1e22d2c40260063d3
am136qa <168574547+am136qa@users.noreply.github.com>
2024-04-30 14:19:27 -0400

    Update Constants.java

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -3,5 +3,5 @@ package utils;
 public class Constants {
     public static final String CONFIG_READER_PATH =System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
     public static final String SCREENSHOT_FILEPATH = System.getProperty("user.dir")+"/screenshots/";
-    public static final String EXCEL_READER_PATH = System.getProperty("user.dir")+"/src/test/resources/testdata/EmployeeDataBatch16.xlsx";
+    public static final String EXCEL_READER_PATH = System.getProperty("user.dir")+"/src/test/resources/testdata/EmployeeData.xlsx";
 }
