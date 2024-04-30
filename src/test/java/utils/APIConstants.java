9520d967b365ee53fb5d06465c7a027591f778e4
am136qa <168574547+am136qa@users.noreply.github.com>
2024-04-30 13:43:00 -0400

    Update APIConstants.java

--- src/test/java/utils/APIConstants.java
+++ src/test/java/utils/APIConstants.java
@@ -3,7 +3,7 @@ package utils;
 import io.restassured.RestAssured;
 
 public class APIConstants {
-    public static final String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
+    public static final String baseURI = RestAssured.baseURI = "http://hrm/api";
     public static final String CREATE_EMPLOYEE_URI = baseURI+"/createEmployee.php";
     public static final String GET_ONE_EMPLOYEE_URI = baseURI+"/getOneEmployee.php";
     public static final String UPDATE_EMPLOYEE_URI = baseURI+"/updateEmployee.php";
