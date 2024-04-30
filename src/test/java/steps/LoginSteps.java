493a65c302585131f1eb620ad535fc66c47d2e4b
am136qa <168574547+am136qa@users.noreply.github.com>
2024-04-30 14:24:45 -0400

    Update LoginSteps.java

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -18,29 +18,17 @@ import utils.Log;
 import java.time.Duration;
 
 public class LoginSteps extends CommonMethods {
-    // public WebDriver driver;
+
     @Given("user is navigated to HRMS application")
     public void user_is_navigated_to_hrms_application() {
-        //to launch chrome browser
         openBrowserAndNavigateToURL();
-        //  driver = new ChromeDriver();
-        //driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login%22);
-        //driver.manage().window().maximize();
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     }
     @When("user enters valid admin username and password")
     public void user_enters_valid_admin_username_and_password() {
-        //creating the object of the class to access all the web elements from it
-        // LoginPage loginPage = new LoginPage();
-        // WebElement usernameField = driver.findElement(By.id("txtUsername"));
-        //  WebElement passwordField = driver.findElement(By.id("txtPassword"));
-        //entering the credentials
-        //  usernameField.sendKeys(ConfigReader.getPropertyValue("username"));
-        //   passwordField.sendKeys(ConfigReader.getPropertyValue("password"));
-        ///we are calling DOMConfigurator which is asking for the file which we used
-        //to integrate logs in our project
+       
         DOMConfigurator.configure("log4j.xml");
-        Log.startTestCase("My batch 16 test case starts here");
+        Log.startTestCase("-----");
         sendText(ConfigReader.getPropertyValue("username"), loginPage.usernameField);
         Log.info("my username has been entered");
         sendText(ConfigReader.getPropertyValue("password"), loginPage.passwordField);
@@ -50,8 +38,7 @@ public class LoginSteps extends CommonMethods {
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
         LoginPage loginPage = new LoginPage();
-        // WebElement loginButton = driver.findElement(By.name("Submit"));
-        //loginButton.click();
+      
         click(loginPage.loginButton);
     }
 
@@ -62,24 +49,15 @@ public class LoginSteps extends CommonMethods {
 
     @When("user enters ess username and password")
     public void user_enters_ess_username_and_password() {
-       // LoginPage loginPage = new LoginPage();
-        //  WebElement usernameField = driver.findElement(By.id("txtUsername"));
-        //  WebElement passwordField = driver.findElement(By.id("txtPassword"));
-        //logged in via normal employee
-        //usernameField.sendKeys("dalima123");
-        //  passwordField.sendKeys("Hum@nhrm123");
-        sendText("dalima123", loginPage.usernameField);
-        sendText("Hum@nhrm123", loginPage.passwordField);
+      
+        sendText("-----3", loginPage.usernameField);
+        sendText("-----", loginPage.passwordField);
     }
     @When("user enters invalid admin username and password")
     public void user_enters_invalid_admin_username_and_password() {
-        // WebElement usernameField = driver.findElement(By.id("txtUsername"));
-        // WebElement passwordField = driver.findElement(By.id("txtPassword"));
-        // usernameField.sendKeys("admin123");
-        //  passwordField.sendKeys("Hum@nhrm123");
-       // LoginPage loginPage = new LoginPage();
-        sendText("admin123", loginPage.usernameField);
-        sendText("Hum#n", loginPage.passwordField);
+       
+        sendText("-----", loginPage.usernameField);
+        sendText("-----", loginPage.passwordField);
     }
 
     @Then("error message is displayed")
@@ -92,9 +70,8 @@ public class LoginSteps extends CommonMethods {
         sendText(username, loginPage.usernameField);
         sendText(password, loginPage.passwordField);
         click(loginPage.loginButton);
-        //fetching the error message from the web element
+   
         String errorMessageActual = loginPage.errorMessageField.getText();
-        //error message coming from feature file too which we can compare
         Assert.assertEquals("value does not match", errorMessageExpected, errorMessageActual);
 
     }
